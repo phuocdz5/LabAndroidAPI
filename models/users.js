0333384a181f2da1b2854456a244490c56e2f2a5
phuocdz5 <phuoccntt2004@gmail.com>
2024-03-27 20:49:56 +0700

    done lab 5

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Scheme = mongoose.Schema;
 
-const Users = new Scheme({
+const userSchema = new Scheme({
     username: { type: String, unique: true, maxLength: 255 },
     password: { type: String, maxLength: 255 },
     email: { type: String },
@@ -11,4 +11,5 @@ const Users = new Scheme({
 }, {
     timestamps: true//tạo ra 2 trường create at và update at
 })
-module.exports = mongoose.model('user', Users)
+const Users = mongoose.model('user', userSchema);
+module.exports = Users
--- routes/api.js
+++ routes/api.js
@@ -282,4 +282,91 @@ router.post('/login', async (req, res) => {
         console.log(error);
     }
 })
+//thêm từ 285 trở xuống 
+router.get('/get-list-distributor', async (_req, res) => {
+    try {
+        const data = await Distributors.find().sort({ createAt: -1 });
+        if (data) {
+            res.json({
+                "status": 200,
+                "messenger": "thành công",
+                "data": data
+            })
+        } else {
+            res.json({
+                "status": 400,
+                "messenger": "Lỗi, không thành công",
+                "data": []
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/search-distributors',async(req,res)=>{
+    try {
+        const key = req.query.key;
+
+        const data = await Distributors.find({name:{"$regex":key, "$options":"i"}}).sort({createdAt:-1});
+        if(data){
+            res.json({
+                "status":200,
+                "messenger":"Thành công",
+                "data":data
+            })
+        }else{
+            res.json({
+                "status":400,
+                "messenger":"Không thành công",
+                "data":[]
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+router.delete('/delete-distributors/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await Distributors.findByIdAndDelete(id);
+        if (result) {
+            res.json({
+                "status": 200,
+                "messenger": "Xóa thành công",
+                "data": result
+            })
+        } else {
+            res.json({
+                "status": 400,
+                "messenger": "Lỗi, xóa không thành công",
+                "data": []
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+router.put('/update-distributors/:id', async(req,res)=>{
+    try {
+        const {id}=req.params;
+        const data = req.body;
+        const result = await Distributors.findByIdAndUpdate(id,{name:data.name});
+        if(result){
+            res.json({
+                "status": 200,
+                "messenger": "Sửa thành công",
+                "data": result
+            })
+        }else{
+            res.json({
+                "status": 400,
+                "messenger": "Lỗi, sửa không thành công",
+                "data": []
+            })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
 module.exports = router
